fb107e6eeac8f90e7568c9d8474a7dc54caf91b5
Karl Forner <karl.forner@gmail.com>
2017-03-01 21:05:58 +0100

    removed getmaxyx2

--- Rcurses/R/coords.r
+++ Rcurses/R/coords.r
@@ -1,15 +1,3 @@
-#' get the current beginning coordinates and size of the default window.
-#'
-#' @return a named list of y, x (or #rows, #cols)
-#' @export
-#' @family coords
-getmaxyx2 <- function() {
-  x <- y <- 0L
-  coords <- .c('_getstdscr_maxyx', y, x)
-  names(coords) <- c('rows', 'cols')
-
-  coords
-}
 
 #' get the current beginning coordinates and size of the default window.
 #'
