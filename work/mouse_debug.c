dde2e94c7a30dd51de39ad435a8ddd0055f41bf5
Karl Forner <karl.forner@gmail.com>
2017-04-06 19:22:32 +0200

    found working C example.

---
+++ work/mouse_debug.c
@@ -0,0 +1,33 @@
+#include <ncurses.h>
+#include <assert.h>
+
+int main() {
+    int ch, count=0;
+    MEVENT event;
+
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+
+    clear();
+    cbreak();
+
+    mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
+
+    while ((ch = getch()) != 'q') {
+        count++;
+        mvprintw(1, 1, "Has mouse: %d", has_mouse());
+        mvprintw(2, 1, "Key code: %x; mouse code:%x", ch, KEY_MOUSE);
+        if (ch == KEY_MOUSE) {
+            assert(getmouse(&event) == OK);
+            mvprintw(3, 3, "Mouse Event: x=%d, y=%d z=%d",
+                    event.x, event.y, event.z);
+            mvprintw(4, 3, "Mouse device id: %x", event.id);
+            mvprintw(5, 3, "Mouse button mask: %x", event.bstate);
+        }
+        mvprintw(6, 1, "Event number: %4d",count);
+        refresh();
+    }
+    endwin();
+}
