4f1b69c99112d5bb88211d5bd8c63f06979a10cf
Karl Forner <karl.forner@gmail.com>
2017-04-12 19:43:37 +0200

    dev on demo echochar.

--- Rcurses/demo/echochar.r
+++ Rcurses/demo/echochar.r
@@ -1,32 +1,20 @@
-gdc <- function() {
+echochar_demo <- function() {
   PAIR_DIGITS <- 1
   PAIR_OTHERS <- 2
   PAIR_FRAMES <- 3
 
   on.exit(endwin())
   initscr()
-  keypad()
 
-  cbreak()
-  noecho()
-  nodelay()
-  #curs_set(0)
 
   if (has_colors()) {
-    bg <- COLOR$BLACK
-
-    res <- try(use_default_colors())
-    if (!inherits(res, 'try-error')) {
-      bg <- -1
-    }
-
-    init_pair(PAIR_DIGITS, COLOR$BLACK, COLOR$RED);
-    init_pair(PAIR_OTHERS, COLOR$RED, bg);
-    init_pair(PAIR_FRAMES, COLOR$WHITE, bg);
-
-    # (void) attrset(AttrArg(COLOR_PAIR(PAIR_OTHERS), 0))
-
+    start_color()
   }
+
+  printw("coucou\n")
+  refresh()
+
+  Sys.sleep(1)
 
 
   clear()
@@ -34,4 +22,4 @@ gdc <- function() {
 }
 
 
-gdc()
+echochar_demo()
--- test_rcurses.r
+++ test_rcurses.r
@@ -7,3 +7,4 @@ test('Rcurses', 'coords')
 test('Rcurses', 'attributes')
 
 source('demo/gdc.r')
+source('demo/echochar.r')
